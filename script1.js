da2fb1147c79c1206916ecca50e993ff0a090e27
zendayat <nmapzendaya@gmail.com>
2023-01-05 11:36:18 +0300

    Initial Release

---
+++ script1.js
@@ -0,0 +1,251 @@
+var user = document.getElementById("user").value;
+var pass = document.getElementById("pass").value;
+function myAccept(){
+        if (user != null){
+            if (pass != null){
+                document.getElementById("acceptance").innerHTML = "Login Successful.Welcome Among the elites.";
+            }
+        }
+        else{
+            document.getElementById("acceptance").innerHTML = "Failed. Please try again.";
+        }
+}
+function mySpawn1(){
+    document.getElementById("speed").innerHTML = "260 Miles/hr";
+    document.getElementById("make").innerHTML = "Mercedes";
+    document.getElementById("engine").innerHTML = "Turbo H6";
+    document.getElementById("seat").innerHTML = "2-leather seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+}
+function mySpawn2(){
+    document.getElementById("speed").innerHTML = "270 Miles/hr";
+    document.getElementById("make").innerHTML = "Audi";
+    document.getElementById("engine").innerHTML = "Max Turbo 2.0 tripple";
+    document.getElementById("seat").innerHTML = "2 Thin-leather Sport seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+}
+function mySpawn3(){
+    document.getElementById("speed").innerHTML = "270 Miles/hr";
+    document.getElementById("make").innerHTML = "Horseyu";
+    document.getElementById("engine").innerHTML = "Turbo H6 Boost";
+    document.getElementById("seat").innerHTML = "2 leaather seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Seat warmer installed";
+    document.getElementById("available").innerHTML = "Call to Check";
+
+}
+function mySpawn4(){
+    document.getElementById("speed").innerHTML = "260 Miles/hr";
+    document.getElementById("make").innerHTML = "Mercedes";
+    document.getElementById("engine").innerHTML = "Turbo 3.2";
+    document.getElementById("seat").innerHTML = "2-leather seater";
+    document.getElementById("DPrice").innerHTML = "$4500";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+
+}
+function mySpawn5(){
+    document.getElementById("speed").innerHTML = "280 Miles/hr";
+    document.getElementById("make").innerHTML = "Lamborghini";
+    document.getElementById("engine").innerHTML = "Max Turbo 2.0 quadripple";
+    document.getElementById("seat").innerHTML = "2 leather seater";
+    document.getElementById("DPrice").innerHTML = "$5100";
+    document.getElementById("perk").innerHTML = "Seat warmer and wifi installed";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function mySpawn6(){
+    document.getElementById("speed").innerHTML = "270 Miles/hr";
+    document.getElementById("make").innerHTML = "Chevlorret";
+    document.getElementById("engine").innerHTML = "Max Turbo 2.0 tripple";
+    document.getElementById("seat").innerHTML = "2 seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "comes with bass speakes";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function mySpawn7(){
+    document.getElementById("speed").innerHTML = "270 Miles/hr";
+    document.getElementById("make").innerHTML = "Lamborgini";
+    document.getElementById("engine").innerHTML = "Max Turbo 2.0 tripple";
+    document.getElementById("seat").innerHTML = "2 seater";
+    document.getElementById("DPrice").innerHTML = "%5,500";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function mySpawns1(){
+    document.getElementById("speed").innerHTML = "211 Miles/hr";
+    document.getElementById("make").innerHTML = "Ferrari SF90";
+    document.getElementById("engine").innerHTML = "four-liter, twin-turbo V8 internal combustion engine ";
+    document.getElementById("seat").innerHTML = "2-leather seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+}
+function mySpawns2(){
+    document.getElementById("speed").innerHTML = "198 Miles/hr";
+    document.getElementById("make").innerHTML = "Audi R8";
+    document.getElementById("engine").innerHTML = "V-10 engine";
+    document.getElementById("seat").innerHTML = "2 Thin-leather Sport seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+}
+function mySpawns3(){
+    document.getElementById("speed").innerHTML = "202 Miles/hr";
+    document.getElementById("make").innerHTML = "Mercedes amg GTR";
+    document.getElementById("engine").innerHTML = "AMG 4.0-litre V8 biturbo";
+    document.getElementById("seat").innerHTML = "2 leaather seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Seat warmer installed";
+    document.getElementById("available").innerHTML = "Call to Check";
+
+}
+function mySpawns4(){
+    document.getElementById("speed").innerHTML = "310 Miles/hr";
+    document.getElementById("make").innerHTML = "koenigsegg jesko";
+    document.getElementById("engine").innerHTML = "twin-turbocharged 5.0-liter V-8";
+    document.getElementById("seat").innerHTML = "2-leather seater";
+    document.getElementById("DPrice").innerHTML = "$5500";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+
+}
+function mySpawns5(){
+    document.getElementById("speed").innerHTML = "199 Miles/hr";
+    document.getElementById("make").innerHTML = "BMW mv8";
+    document.getElementById("engine").innerHTML = "twin-turbo 4.4-liter V-8";
+    document.getElementById("seat").innerHTML = "2 leather seater";
+    document.getElementById("DPrice").innerHTML = "$4000";
+    document.getElementById("perk").innerHTML = "Seat warmer and wifi installed";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function mySpawns6(){
+    document.getElementById("speed").innerHTML = "206 Miles/hr";
+    document.getElementById("make").innerHTML = "Ferrari 488 GTE";
+    document.getElementById("engine").innerHTML = "V8, Turbocharged";
+    document.getElementById("seat").innerHTML = "2 seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "comes with bass speakers";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function mySpawns7(){
+    document.getElementById("speed").innerHTML = "168 Miles/hr";
+    document.getElementById("make").innerHTML = "Lexux";
+    document.getElementById("engine").innerHTML = "Max Turbo 2.0 tripple";
+    document.getElementById("seat").innerHTML = "2 seater";
+    document.getElementById("DPrice").innerHTML = "%5,500";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function mySpawns8(){
+    document.getElementById("speed").innerHTML = "270 Miles/hr";
+    document.getElementById("make").innerHTML = "Lamborgini";
+    document.getElementById("engine").innerHTML = "Max Turbo 2.0 tripple";
+    document.getElementById("seat").innerHTML = "2 seater";
+    document.getElementById("DPrice").innerHTML = "%5,500";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function MySpawns1(){
+    document.getElementById("speed").innerHTML = "125 Miles/hr";
+    document.getElementById("make").innerHTML = "Jeep";
+    document.getElementById("engine").innerHTML = "6.4L HEMI® V8  ";
+    document.getElementById("seat").innerHTML = "5 seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+}
+function MySpawns2(){
+    document.getElementById("speed").innerHTML = "140 Miles/hr";
+    document.getElementById("make").innerHTML = "Jeep";
+    document.getElementById("engine").innerHTML = "6.4L HEMI® V8 ";
+    document.getElementById("seat").innerHTML = "5 seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+}
+function MySpawns3(){
+    document.getElementById("speed").innerHTML = "125 Miles/hr";
+    document.getElementById("make").innerHTML = "Jeep";
+    document.getElementById("engine").innerHTML = "6.4L HEMI® V8 ";
+    document.getElementById("seat").innerHTML = "5 seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "Seat warmer installed";
+    document.getElementById("available").innerHTML = "Call to Check";
+
+}
+function MySpawns4(){
+    document.getElementById("speed").innerHTML = "135 Miles/hr";
+    document.getElementById("make").innerHTML = "Jeep";
+    document.getElementById("engine").innerHTML = "6.4L HEMI® V8 ";
+    document.getElementById("seat").innerHTML = "5 seater";
+    document.getElementById("DPrice").innerHTML = "$5500";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to Check";
+
+}
+function MySpawns5(){
+    document.getElementById("speed").innerHTML = "150 Miles/hr";
+    document.getElementById("make").innerHTML = "Jeep";
+    document.getElementById("engine").innerHTML = "6.4-liter V-8 ";
+    document.getElementById("seat").innerHTML = "5 seater";
+    document.getElementById("DPrice").innerHTML = "$4000";
+    document.getElementById("perk").innerHTML = "Seat warmer and wifi installed";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function MySpawns6(){
+    document.getElementById("speed").innerHTML = "135 Miles/hr";
+    document.getElementById("make").innerHTML = "Jeep";
+    document.getElementById("engine").innerHTML = "6.4-liter V-8 ";
+    document.getElementById("seat").innerHTML = "5 seater";
+    document.getElementById("DPrice").innerHTML = "$5000";
+    document.getElementById("perk").innerHTML = "comes with bass speakers";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function MySpawns7(){
+    document.getElementById("speed").innerHTML = "150 Miles/hr";
+    document.getElementById("make").innerHTML = "Jeep";
+    document.getElementById("engine").innerHTML = "6.4-liter V-8 ";
+    document.getElementById("seat").innerHTML = "5 seater";
+    document.getElementById("DPrice").innerHTML = "%5,500";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+function MySpawns8(){
+    document.getElementById("speed").innerHTML = "120 Miles/hr";
+    document.getElementById("make").innerHTML = "Jeep";
+    document.getElementById("engine").innerHTML = "6.4-liter V-8 ";
+    document.getElementById("seat").innerHTML = "5 seater";
+    document.getElementById("DPrice").innerHTML = "%5,500";
+    document.getElementById("perk").innerHTML = "Comes with wifi";
+    document.getElementById("available").innerHTML = "Call to confirm";
+
+}
+
+var menuList = document.getElementById("menuList");
+
+menuList.style.maxHeight = "0px";
+
+function ToggleMenu() {
+    if(menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "75px";
+    }
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+}
+
+
